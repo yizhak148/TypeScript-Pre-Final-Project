76e3147611c8c8df895a8708cceb06e38c5152db
yizhak148 <yizhak208@gmail.com>
2023-11-20 18:55:49 +0200

    btn

--- app.ts
+++ app.ts
@@ -1,2 +1,21 @@
 const myForm = document.querySelector("form[activity-tracker]")
-console.log(myForm);
+
+const activityNameinput = document.getElementById("activity-name") as HTMLInputElement;
+const activitytimeinput = document.getElementById("activity-time") as HTMLInputElement;
+
+const savebtn = document.querySelector("button")?.addEventListener("click", uptadejson)
+
+function uptadejson(){
+
+    const useractivityname = activityNameinput.value
+    const useractivityTime = activitytimeinput.value
+    
+    const namejson = JSON.stringify(useractivityname)
+    const timejson = JSON.stringify(useractivityTime)
+    sessionStorage.setItem("activity name", namejson)
+    sessionStorage.setItem("activity time", timejson)
+}
+
+uptadejson()
+
+
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
+var _a;
 var myForm = document.querySelector("form[activity-tracker]");
-console.log(myForm);
+var activityNameinput = document.getElementById("activity-name");
+var activitytimeinput = document.getElementById("activity-time");
+var savebtn = (_a = document.querySelector("button")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", uptadejson);
+function uptadejson() {
+    var useractivityname = activityNameinput.value;
+    var useractivityTime = activitytimeinput.value;
+    var namejson = JSON.stringify(useractivityname);
+    var timejson = JSON.stringify(useractivityTime);
+    sessionStorage.setItem("activity name", namejson);
+    sessionStorage.setItem("activity time", timejson);
+}
+uptadejson();
